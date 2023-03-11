789f4829e6cdaae183b013a78925498da6d17e72
alfrdmalr <alfrdmalr@gmail.com>
2023-03-11 17:27:52 -0500

    chore: rm wip result constructs

--- src/types/numberish.tsx
+++ src/types/numberish.tsx
@@ -4,29 +4,3 @@ export type Numberish = number | null;
 export function isValid(n: Numberish): n is number {
   return !(n !== 0 && !n);
 }
-
-export type Result<T = unknown, E = unknown> = OkResult<T> | ErrResult<E>;
-
-type OkResult<T> = {
-  isOk: true;
-  success: T;
-};
-
-export function Ok<T>(arg: T): OkResult<T> {
-  return {
-    isOk: true,
-    success: arg,
-  };
-}
-
-export function Err<E>(error: E): ErrResult<E> {
-  return {
-    isOk: false,
-    error: error,
-  };
-}
-
-type ErrResult<E> = {
-  isOk: false;
-  error: E;
-};
